adceaa18a065c5dce5a386b5abff658474100041
Mohammod Ali <mohammodali651@gmail.com>
2024-01-17 21:40:14 +0600

    install react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,19 @@ import MainLayout from "./components/layout/MainLayout";
 import Button from "./components/ui/Button";
 import Container from "./components/ui/Container";
 import Modal from "./utils/Modal";
+import NormalForm from "./components/NormalForm/NormalForm";
 
 function App() {
 
-  const [modal, setModal] = useState(false)
+  // const [modal, setModal] = useState(false)
 
-  const handleModalClose = () => {
-    setModal((prev) => !prev)
-  }
+  // const handleModalClose = () => {
+  //   setModal((prev) => !prev)
+  // }
 
   return (
    <Container>
-    <div className="h-screen w-full flex justify-center items-center">
-      {/* <button className="btn btn-primary">This is a button</button>
-      <button className="btn btn-danger">This is a button</button> */}
-      <Button onClick={() => setModal((prev) => !prev)}>Open Modal</Button>
-      <Modal isOpen={modal} onClose={handleModalClose}></Modal>
-    </div>
-    
+    <NormalForm></NormalForm>
    </Container>
   );
 }
