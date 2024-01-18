2c3c405c42f730025ab5e92346eab65237a26813
Mohammod Ali <mohammodali651@gmail.com>
2024-01-18 23:01:48 +0600

    setup tailwind forms

--- src/components/NormalForm/NormalForm.tsx
+++ src/components/NormalForm/NormalForm.tsx
@@ -12,12 +12,27 @@ const NormalForm = () => {
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <div>
-                <label htmlFor="name">Name </label>
+                <label className='block' htmlFor="name">Name </label>
                 <input type="text" id="name" {...register('name')} />
             </div>
+            <div>
+                <label className='block' htmlFor="email">Email </label>
+                <input type="email" id="email" {...register('email')} />
+            </div>
+            <div>
+                <label className='block' htmlFor="password">Password </label>
+                <input type="password" id="password" {...register('password')} />
+            </div>
+            
+            <select name="" id="">
+                <option value="">one</option>
+                <option value="">two</option>
+                <option value="">three</option>
+                <option value="">four</option>
+            </select>
             <button type='submit'>Submit</button>
         </form>
     );
 };
 
-export default NormalForm;
+export default NormalForm; 
